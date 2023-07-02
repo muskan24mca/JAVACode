aadeb4a32d883cf2b8e3b66d988466dfc0ad3759
Muskan <104146101+muskan24mca@users.noreply.github.com>
2023-07-02 12:32:13 +0530

    Add files via upload

---
+++ Ques5.java
@@ -0,0 +1,11 @@
+package Mock;
+import java.util.*;
+
+public class Ques5 {
+   public static void main(String[] args) {
+    int age = 23;
+    double salary= 50000.0;
+    String name = "Muskan";
+    System.out.println(name + " " + age + " " + salary);
+   }
+}
