54dfd3cad50873657e3d713a072ef7e7739112ca
Muskan <104146101+muskan24mca@users.noreply.github.com>
2023-07-09 19:16:11 +0530

    Add files via upload

---
+++ Ques13.java
@@ -0,0 +1,45 @@
+package Mock;
+
+class Person{
+    private String name;
+    private int age;
+    private String address;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
+public class Ques13 {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.setName("Muskan");
+        person.setAge(23);
+        person.setAddress("123 Main Street, City");
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Address: " + person.getAddress());
+    }
+}
